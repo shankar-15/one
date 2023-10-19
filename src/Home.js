e3a6978e618394b7032941c126225c7ce3e54598
shreya <shreyashankar.bko@gmail.com>
2023-10-19 17:28:14 +0530

    changes Home Services App

--- src/Home.js
+++ src/Home.js
@@ -2,14 +2,19 @@
 import React from 'react';
 import './App.css';
 
+
 function Home() {
   return (
     <div className="home">
-      <div className="blur-square">
+      <div className='container-home'>
+        <div className='glass'>
         <h1>Blurred Square</h1>
         <p>This is some text on a blurred square.</p>
       </div>
     </div>
+    
+    </div>
+    
   );
 }
 
--- src/Product.js
+++ src/Product.js
@@ -1,15 +1,18 @@
 // ProductCard.js
 import React from 'react';
 import './ProductCrd.css'; // Import the CSS for styling
+import Header from './Header';
 
 function Products({ product }) {
   return (
+    
     <div className="product-card">
       <img src={product.image} alt={product.name} />
       <h3>{firesensor}</h3>
       <p>auto detection of fire within few seconds</p>
       <p>Price: ${200}</p>
     </div>
+    
   );
 }
 
