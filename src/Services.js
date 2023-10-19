3b8d38b5102b477022818d0037c34a2d166a48a7
shreya <shreyashankar.bko@gmail.com>
2023-10-19 17:30:10 +0530

    modified app.css and Services

--- src/Services.js
+++ src/Services.js
@@ -6,34 +6,29 @@ import './App.css';
 function Services() {
   return (
     <div className ="services" >
-      <div className="box">
-        <div className="background-image-services"></div>
+       <div className='"glass"'>
         <div className="content">
           <div className="text">
             {/* Your content for the Services page */}
           </div>
-          
           <html lang="en">
 <head>
     {/* <meta charset="UTF-8"> */}
     <title>Services</title>
 </head>
 <body>
-    <div class="container">
+    <div class="container-service">
         <div class="scroll-box">
             <h1>Our Services</h1>
             <p>This is the content of our services.</p>
-            <image>
-              <img></img>
-            </image>
-            
         </div>
     </div>
 </body>
 </html>
 </div>
       </div>
-    </div>
+      </div>
+    
     
     
     
