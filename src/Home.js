ed4b87be4929aa2905ba137212d60f1901968305
shreya <shreyashankar.bko@gmail.com>
2023-10-19 17:47:19 +0530

    try to have a box in home page with image(help.jpeg) in it and a blur box in services

--- src/Home.js
+++ src/Home.js
@@ -6,15 +6,9 @@ import './App.css';
 function Home() {
   return (
     <div className="home">
-      <div className='container-home'>
-        <div className='glass'>
         <h1>Blurred Square</h1>
         <p>This is some text on a blurred square.</p>
       </div>
-    </div>
-    
-    </div>
-    
   );
 }
 
